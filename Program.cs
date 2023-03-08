d275d41a710fce638c0f5c253e68a3956d07b9bc
Sagarxox <109059306+Sagarxox@users.noreply.github.com>
2023-03-08 23:40:40 +0530

    [Sagar] Add: Ability to add new Contact

--- Program.cs
+++ Program.cs
@@ -12,10 +12,33 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the address Book System Problem");
+
+            Console.WriteLine("Enter the First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter the Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter the City");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter the State");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Zip");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Phone Number");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter the Email");
+            string email = Console.ReadLine();
+
             AddressBookMain addressBookMain = new AddressBookMain();
-            addressBookMain.AddContact("sagar", "katahre", "yuuu", "pune", "MH", 441877, 01234456789, "abc@gmail.com");
+
+            AddressBookMain.AddContact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            
+
             addressBookMain.PrintList();
             Console.ReadLine();
         }
+
     }
 }
+
